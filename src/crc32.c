d7306ebd2436b292ada133ff28d6874c8d4f75ab
gorazdko <gorazdko@gmail.com>
2020-07-31 18:15:28 +0200

    fix memory leak and arduino support

--- src/crc32.c
+++ src/crc32.c
@@ -6,7 +6,6 @@
 //
 
 #include "crc32.h"
-#include <memory.h>
 
 #ifdef ARDUINO
 #define htonl(x) __builtin_bswap32((uint32_t) (x))
@@ -15,17 +14,14 @@
 #endif
 
 uint32_t crc32(const uint8_t* bytes, size_t len) {
-    static uint32_t* table = NULL;
 
-    if(table == NULL) {
-        table = malloc(256 * sizeof(uint32_t));
-        for(int i = 0; i < 256; i++) {
-            uint32_t c = i;
-            for(int j = 0; j < 8; j++) {
-                c = (c % 2 == 0) ? (c >> 1) : (0xEDB88320 ^ (c >> 1));
-            }
-            table[i] = c;
+    uint32_t table[256];
+    for(int i = 0; i < 256; i++) {
+        uint32_t c = i;
+        for(int j = 0; j < 8; j++) {
+            c = (c % 2 == 0) ? (c >> 1) : (0xEDB88320 ^ (c >> 1));
         }
+        table[i] = c;
     }
 
     uint32_t crc = ~0;
